60d35ed2576db35711c5b0d1bc052fd9a2c273fc
Ivan Antsiferov <antsiferov.ivan@gmail.com>
2017-07-12 14:42:19 +0300

    Подключил LeakCanary.

--- app/src/main/java/yamblz/voltek/com/weather/WeatherApp.java
+++ app/src/main/java/yamblz/voltek/com/weather/WeatherApp.java
@@ -2,4 +2,17 @@ package yamblz.voltek.com.weather;
 
 import android.app.Application;
 
-public class WeatherApp extends Application {}
+import com.squareup.leakcanary.LeakCanary;
+
+public class WeatherApp extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+    }
+}
