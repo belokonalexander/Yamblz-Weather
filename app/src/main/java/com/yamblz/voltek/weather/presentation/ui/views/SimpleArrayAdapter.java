e31e01a2dfcf5b6b9c300cb90ef81378b4589b21
Alexander <belokon202@gmail.com>
2017-07-27 17:42:56 +0300

    1) suggestions changes on a fly

--- app/src/main/java/com/yamblz/voltek/weather/presentation/ui/views/SimpleArrayAdapter.java
+++ app/src/main/java/com/yamblz/voltek/weather/presentation/ui/views/SimpleArrayAdapter.java
@@ -3,6 +3,7 @@ package com.yamblz.voltek.weather.presentation.ui.views;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.Filterable;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.List;
 public class SimpleArrayAdapter<T> extends ArrayAdapter<T> implements Filterable {
 
     private List<T> list;
+    private CustomFilter customFilter;
 
     public SimpleArrayAdapter(Context context, int textViewResourceId, List<T> list) {
         super(context, textViewResourceId);
@@ -45,6 +47,43 @@ public class SimpleArrayAdapter<T> extends ArrayAdapter<T> implements Filterable
     @Override
     public T getItem(int position) {
         return list.get(position);
+    }
+
+    @Override
+    public CustomFilter getFilter() {
+        if (customFilter == null) {
+            customFilter = new CustomFilter();
+        }
+        return customFilter;
+    }
+
+    public void callFiltering(String term) {
+        customFilter.performFiltering(term);
+    }
+
+
+    private class CustomFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+            if (constraint != null) {
+                results.values = list;
+                results.count = list.size();
+            }
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint,
+                                      FilterResults results) {
+            if (results != null && results.count > 0) {
+                notifyDataSetChanged();
+            } else {
+                notifyDataSetInvalidated();
+            }
+        }
+
     }
 
 
